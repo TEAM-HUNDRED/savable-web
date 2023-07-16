5ab90d33d100642bd73f03787ae2091820b8b199
yeongjunekoh <rhdudwns0@naver.com>
2023-07-17 06:39:48 +0900

    [FEATURE] Generate Route structure

---
+++ src/container/LayoutContainer/index.tsx
@@ -0,0 +1,5 @@
+function LayoutContainer() {
+  return <>안녕</>;
+}
+
+export default LayoutContainer;
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import HomePage from "pages/HomePage";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
 
