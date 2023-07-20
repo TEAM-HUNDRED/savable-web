0a6cd4742099eb2c476a19829e55dd68658403dc
yeongjunekoh <rhdudwns0@naver.com>
2023-07-21 07:51:17 +0900

    [SETTING] add dotenv library
    - fix issue in reacct-script v>5

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,6 +1,10 @@
 import styled from "styled-components";
 
 import SavingHeader from "components/SavingHeader";
+import dotenv from "dotenv";
+dotenv.config();
+
+console.log(process.env.REACT_APP_API_BASE_URL);
 
 function HomePage() {
   return (
