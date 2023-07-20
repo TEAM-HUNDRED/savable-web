d43e278e57da53c9bf745ab49c9bc5c7fecb29e7
yeongjunekoh <rhdudwns0@naver.com>
2023-07-21 03:59:03 +0900

    [FEATURE] Generate ui ranking bar

---
+++ src/components/RankingBar/index.tsx
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+
+type TypeProps = {
+  rank: number;
+  userName: string;
+  highlighted: boolean;
+};
+
+function RankingBar({ rank, userName, highlighted }: TypeProps) {
+  return (
+    <Container highlighted={highlighted}>
+      <Text>{`${rank}위`}</Text>
+      <Divider />
+      <Text>{`${userName}님`}</Text>
+    </Container>
+  );
+}
+
+const Container = styled.div<{ highlighted: boolean }>`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  border: 1px solid #e8e8e8;
+  border-radius: 16px;
+  padding: 16px;
+  margin-bottom: 10px;
+  align-items: center;
+  background-color: ${(props) => (props.highlighted ? "#E9F9AC" : "#fff")};
+  box-sizing: border-box;
+`;
+
+const Divider = styled.div`
+  height: 14px;
+  width: 1px;
+  background-color: #cfcfcf;
+  margin: 0px 10px;
+`;
+
+const Text = styled.p`
+  font-size: 16px;
+  font-weight: 700;
+  margin: 0px;
+`;
+
+export default RankingBar;
--- src/pages/RankingPage/index.tsx
+++ src/pages/RankingPage/index.tsx
@@ -1,15 +1,48 @@
 import styled from "styled-components";
 
 import RankingHeader from "components/RankingHeader";
+import RankingBar from "components/RankingBar";
 
 function RankingPage() {
   const userName = "고영준";
+
+  const dummyRankList = [
+    { userName: "asdf" },
+    { userName: "asdf" },
+    { userName: "asdf" },
+    { userName: "asdf" },
+    { userName: "고영준" },
+    { userName: "asdf" },
+    { userName: "asdf" },
+    { userName: "asdf" },
+    { userName: "asdf" },
+    { userName: "asdf" },
+    { userName: "asdf" },
+    { userName: "asdf" },
+    { userName: "asdf" },
+    { userName: "asdf" },
+    { userName: "asdf" },
+    { userName: "asdf" },
+    { userName: "asdf" },
+    { userName: "asdf" },
+    { userName: "asdf" },
+    { userName: "asdf" },
+  ];
 
   return (
     <Container>
       <TitleText>{`세이버 ${userName}의 이번 주 랭킹`}</TitleText>
       <RankingHeader />
       <Divider />
+      {dummyRankList.map((item, idx) => {
+        return (
+          <RankingBar
+            userName={item.userName}
+            rank={idx + 1}
+            highlighted={item.userName === userName}
+          />
+        );
+      })}
     </Container>
   );
 }
@@ -29,6 +62,7 @@ const Divider = styled.div`
   width: 100%;
   height: 2px;
   background-color: #e3e3e3;
+  margin-bottom: 16px;
 `;
 
 export default RankingPage;
